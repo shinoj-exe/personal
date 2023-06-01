2ee7e2c4b0eb4929d2ee2d9f8a5aae3c46adb108
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-02 00:25:11 +0530

    navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,52 @@
 import { logo } from '@/public/assets'
 import Image from 'next/image'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className="w-full shadow-navbarShadow h-20 1g:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-titlefont flex items-center justify-between ">
-        <div>
+        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.5}}>
           {/* <Image className='w-14' src={logo} alt="Logo" /> */}
           <span className='text-base md:text-lg text-textGreen '>SHINOJ</span>
+        </motion.div>
+        <div className="hidden mdl:inline-flex items-center gap-7">
+          <ul className='flex text-[13px] gap-7'>
+            <Link  href="#home" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li initial={{ y: -10, opacity: 0 }}
+                       animate={{ y: 0, opacity: 1 }}
+                       transition={{ duration: 0.1 }}>Home</motion.li>
+            </Link>
+            <Link  href="#about" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li initial={{ y: -10, opacity: 0 }}
+                       animate={{ y: 0, opacity: 1 }}
+                       transition={{ duration: 0.1,delay:0.1 }}><span className='text-textGreen'>0.1</span> About</motion.li>
+            </Link>
+            <Link  href="#experience" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li initial={{ y: -10, opacity: 0 }}
+                       animate={{ y: 0, opacity: 1 }}
+                       transition={{ duration: 0.1,delay:0.2 }}><span className='text-textGreen'>0.2</span> Experience</motion.li>
+            </Link>
+            <Link  href="#project" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li initial={{ y: -10, opacity: 0 }}
+                       animate={{ y: 0, opacity: 1 }}
+                       transition={{ duration: 0.1 ,delay:0.3}}><span className='text-textGreen'>0.3</span> Project</motion.li>
+            </Link>
+            <Link  href="#contact" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li initial={{ y: -10, opacity: 0 }}
+                       animate={{ y: 0, opacity: 1 }}
+                       transition={{ duration: 0.1,delay:0.4}}><span className='text-textGreen'>0.4</span> Contact</motion.li>
+            </Link>
+          </ul>
+          <a href="/assets/Resume_Shinoj_Muraleedharan.pdf" target='_blank'>
+
+          <motion.button initial={{  opacity: 0 }}
+                       animate={{ opacity: 1 }}
+                       transition={{ delay: 0.5 }} className="px-4 py-2 rounded-md text-textGreen text-[13px] border  border-textGreen hover:bg-hoverColor duration-300">Resume</motion.button>
+          </a>
         </div>
-        <div></div>
       </div>
     </div>
   )
