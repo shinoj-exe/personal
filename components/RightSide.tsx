96bf596c0425179aef1136613c9f3bc6e245aede
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-04 00:51:02 +0530

    custom scrollbar

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -4,7 +4,7 @@ const RightSide = () => {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end gap-14 text-textLight'>
         <a href="mailto:shinojmuralee@gmail.com">
-            <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen'>shinojmuralee@gmail.com</p>
+            <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen hover:-translate-y-6 transition-all duration:300'>shinojmuralee@gmail.com</p>
         </a>
         <span className='w-[2px] h-32 bg-textDark inline-flex' ></span>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     <Head>
       <title>Shinoj Muralee</title>
     </Head>
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
       {/* New portfolio */}
       <Navbar/>
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
