7e5be3aad5e49afdaf6c4a9bc295dbae2e17b67d
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-04 10:41:36 +0530

    update scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,24 @@ import { logo } from '@/public/assets'
 import Image from 'next/image'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-import React from 'react'
+import React,{useRef} from 'react'
 
 const Navbar = () => {
+  const ref=useRef<string | any>("");
+  const handleScroll=(e: React.MouseEvent<HTMLAnchorElement,MouseEvent>)=>{
+    e.preventDefault()
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/,"");
+    const elem=document.getElementById(targetId)
+    elem?.scrollIntoView({
+      behavior:"smooth",
+    });
+    const links = document.querySelectorAll(".nav-link")
+    links.forEach((link)=>{
+      link.classList.remove("active")
+    });
+    e.currentTarget.classList.add("active")
+  }
   return (
     <div className="w-full shadow-navbarShadow h-20 1g:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-titlefont flex items-center justify-between ">
@@ -14,27 +29,27 @@ const Navbar = () => {
         </motion.div>
         <div className="hidden mdl:inline-flex items-center gap-7">
           <ul className='flex text-[12px] gap-7'>
-            <Link  href="#home" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
-            <motion.li initial={{ y: -10, opacity: 0 }}
+            <Link onClick={handleScroll} href="#home" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <motion.li  initial={{ y: -10, opacity: 0 }}
                        animate={{ y: 0, opacity: 1 }}
                        transition={{ duration: 0.1 }}>Home</motion.li>
             </Link>
-            <Link  href="#about" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <Link  onClick={handleScroll} href="#about" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
             <motion.li initial={{ y: -10, opacity: 0 }}
                        animate={{ y: 0, opacity: 1 }}
                        transition={{ duration: 0.1,delay:0.1 }}><span className='text-textGreen'>0.1</span> About</motion.li>
             </Link>
-            <Link  href="#experience" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <Link  onClick={handleScroll} href="#experience" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
             <motion.li initial={{ y: -10, opacity: 0 }}
                        animate={{ y: 0, opacity: 1 }}
                        transition={{ duration: 0.1,delay:0.2 }}><span className='text-textGreen'>0.2</span> Experience</motion.li>
             </Link>
-            <Link  href="#project" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <Link onClick={handleScroll}  href="#project" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
             <motion.li initial={{ y: -10, opacity: 0 }}
                        animate={{ y: 0, opacity: 1 }}
                        transition={{ duration: 0.1 ,delay:0.3}}><span className='text-textGreen'>0.3</span> Project</motion.li>
             </Link>
-            <Link  href="#contact" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
+            <Link  onClick={handleScroll} href="#contact" className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link">
             <motion.li initial={{ y: -10, opacity: 0 }}
                        animate={{ y: 0, opacity: 1 }}
                        transition={{ duration: 0.1,delay:0.4}}><span className='text-textGreen'>0.4</span> Contact</motion.li>
