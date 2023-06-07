7ec4e3061025c061b58996f5317f98a52747a9f3
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-07 19:10:54 +0530

    updates

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <section id='home' className="max-w-contentContainer mx-auto py-10 mdl : py-24 flex flex-col gap-4 lgl : gap-8 mdl : px-10 xl : px-4">
         <motion.h3 initial={{y : 10, opacity : 0 }} animate={{ y : 0, opacity : 1 }} transition={{ duration : 0.5, delay : 0.6}} className="text-lg font-titleFont tracking-wide text-textGreen" >Hi, my name is</motion.h3>
-        <motion.h1 initial={{y : 10, opacity : 0 }} animate={{ y : 0, opacity : 1 }} transition={{ duration : 0.5, delay : 0.7}} className=" text-4xl  lgl:text-6xl font-titleFont font-semibold flex flex-col">Shinoj Muraleedharan <span className="text-textDark mt-2 lgl : mt-4"> Fullstack Web Devloper.</span> </motion.h1>
+        <motion.h1 initial={{y : 10, opacity : 0 }} animate={{ y : 0, opacity : 1 }} transition={{ duration : 0.5, delay : 0.7}} className=" text-4xl  lgl:text-6xl font-titleFont font-semibold flex flex-col">Shinoj Muraleedharan <span className="text-textDark mt-2 lgl : mt-4"> Web Devloper | Blockchain Developer.</span> </motion.h1>
         <motion.p initial={{y : 10, opacity : 0 }} animate={{ y : 0, opacity : 1 }} transition={{ duration : 0.5, delay : 0.8}} className="text-base md : max-w-[650px] text-textDark font-medium">
             {" "}
              I have a comprehensive understanding of both frontend and backend development, allowing me to create dynamic and interactive web applications. With expertise in HTML, CSS, and JavaScript, I am capable of crafting visually appealing and responsive user interfaces. 
